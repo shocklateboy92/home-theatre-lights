c589e08bec703894adfa8d950c9090e4e276b4b6
Lasath Fernando <kde@lasath.org>
2017-03-22 03:04:29 -0700

    Kind of get smartthings integration to work

--- rest.js
+++ rest.js
@@ -12,32 +12,48 @@ function authFetch(url, callback) {
             }
         }
     )
-    .then(callback)
-    .then(response => console.log(response));
+    .then(response => response.json())
+    .then(callback);
+}
+
+let endpoint = '';
+
+function updateEndpoints() {
+    authFetch(
+        'https://graph.api.smartthings.com/api/smartapps/endpoints',
+        response => {
+            console.log(JSON.stringify(response, null, 4));
+            endpoint = response[0].uri;
+        }
+    );
 }
 
 exports.init = () => {
-    const url = 'https://graph.api.smartthings.com/oauth/token';
-    fetch (url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        body: `grant_type=authorization_code&code=Zz9Axl&client_id=77363fd9-e920-4e85-aa81-dca4f5ec044a&client_secret=b9975d03-e21e-4f95-ac46-6a399c9615ae&redirect_uri=http://localhost:4567/oauth/callback`
-    })
-    .then(response => response.json())
-    .then(json => {
-        console.log(json);
-        token = json.access_token;
+    // const url = 'https://graph.api.smartthings.com/oauth/token';
+    // fetch (url, {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/x-www-form-urlencoded'
+    //     },
+    //     body: `grant_type=authorization_code&code=Zz9Axl&client_id=77363fd9-e920-4e85-aa81-dca4f5ec044a&client_secret=b9975d03-e21e-4f95-ac46-6a399c9615ae&redirect_uri=http://localhost:4567/oauth/callback`
+    // })
+    // .then(response => response.json())
+    // .then(json => {
+    //     console.log(json);
+    //     token = json.access_token;
 
-        // authFetch(
-        //     'https://graph.api.smartthings.com/api/smartapps/endpoints',
-        //     response => {
-        //         console.log(response);
-        //     });
-    });
+    //     // authFetch(
+    //     //     'https://graph.api.smartthings.com/api/smartapps/endpoints',
+    //     //     response => {
+    //     //         console.log(response);
+    //     //     });
+    // });
+    updateEndpoints();
 }
 
 exports.dimLights = () => {
-
+    authFetch(
+        endpoint + '/dim',
+        result => console.log(`>>> successfully made dim lights request: ${JSON.stringify(result)}`)
+    );
 }
